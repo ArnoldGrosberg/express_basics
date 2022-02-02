d658827c29f9455b37b5d38433774347222b2a25
Arnold Grosberg <arnold.grosberg@khk.ee>
2022-02-02 15:22:42 +0200

    post request use

--- index.js
+++ index.js
@@ -4,19 +4,26 @@ const app = express();
 // require path
 const path = require('path')
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/questions', (req, res) => {
-
-	let questions = [
-	{title: "What is Node.js?", user: "Kadi", votes: "10"},
-	{title: "What is Express.js?", user: "Mike", votes: "8"}
-	]
-
-	res.render('index', {questions:questions})
+app.get('/login', (req, res) => {
+	res.render('login')
 })
 
+app.post('/login', (req,res) => {
+	let username = req.body.username;
+	let password = req.body.password;
+	if(username === 'user' && password === 'qwerty'){
+		res.redirect('/dashboard');
+	}
+});
+app.get('/dashboard', (req,res)=>{
+	res.render('dashboard');
+})
 // listen app via port
 app.listen(3000, ()=> {
 	console.log('Server started on http://localhost:3000');
